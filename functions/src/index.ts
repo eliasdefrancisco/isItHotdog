97359290f8de0cd100c57374b7ca4f88ce08bf19
Elias de Francisco <eliasdefrancisco@gmail.com>
2019-09-03 16:57:33 +0200

    Firebase Function added, backend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,39 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Firebase
+import * as admin from 'firebase-admin'
+admin.initializeApp(functions.config().firebase)
+
+// Cloud Vision
+// import * as vision from '@google-cloud/vision'
+const vision = require('@google-cloud/vision')
+const visionClient = new vision.ImageAnnotatorClient()
+
+// Dedicated bucket for cloud function invocation
+const bucketName = 'is-it-hotdog'
+
+export const imageTagger = functions.storage
+.bucket(bucketName)
+.object()
+.onFinalize( async (object: any) => {
+
+    // File data
+    const filePath = object.name
+
+    // Location of saved file in bucket
+    const imageUri = `gs://${bucketName}/${filePath}`
+
+    // Firestore docID === file name
+    const docId = filePath.split('.jpg')[0]
+
+    const docRef = admin.firestore().collection('photos').doc(docId)
+
+    // Await the cloud vision response
+    const results = await visionClient.labelDetection(imageUri)
+
+    // Map the data to desired format
+    const labels = results[0].labelAnnotations.map((obj: any) => obj.description)
+    const hotdog = labels.includes('hot dog')
+
+    return docRef.set({hotdog, labels})
+})
